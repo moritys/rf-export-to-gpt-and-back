b84a746ac9a421f7c4155ded23afa49ff1bdd4d3
Maria Sharkova <moriko.tys@gmail.com>
2024-09-06 17:43:29 +0300

    1st working ver

--- app/claude_connection.py
+++ app/claude_connection.py
@@ -7,21 +7,24 @@ load_dotenv()
 client = anthropic.Anthropic()
 client.api_key = os.getenv('ANTHROPIC_API_KEY')
 
-message = client.messages.create(
-    model="claude-3-5-sonnet-20240620",
-    max_tokens=1000,
-    temperature=0,
-    system="You are a world-class poet. Respond only with short poems.",
-    messages=[
-        {
-            "role": "user",
-            "content": [
-                {
-                    "type": "text",
-                    "text": "Why is the ocean salty?"
-                }
-            ]
-        }
-    ]
-)
-print(message.content)
+
+def send_message(prompt, text):
+    message = client.messages.create(
+        model="claude-3-5-sonnet-20240620",
+        max_tokens=1000,
+        temperature=0,
+        system=prompt,
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": text
+                    }
+                ]
+            }
+        ]
+    )
+    formatted_output = "\n".join(text_block.text for text_block in message.content if hasattr(text_block, 'text'))
+    return formatted_output
